bc7da0248622cfd5904202d2956b7e953dbe5a85
Luc <luc2411b@gmail.com>
2022-03-11 13:43:41 +0100

    menu.js gemaakt

---
+++ menu.js
@@ -0,0 +1,7 @@
+class Menu {
+    constructor (sections) {
+        this.sections = sections;
+
+var section1 = new Menu("Pizza");
+var section2 = new Menu("Snacks");
+var section3 = new Menu("Drinks");
