389e0eabf9a5e60565bd639beea6cd35e1b62819
Luc <luc2411b@gmail.com>
2022-03-11 13:57:24 +0100

    update menujs

--- menu.js
+++ menu.js
@@ -1,7 +1,14 @@
 class Menu {
     constructor (sections) {
         this.sections = sections;
+    }
+}
 
-var section1 = new Menu("Pizza");
-var section2 = new Menu("Snacks");
-var section3 = new Menu("Drinks");
+
+var section1 = new Menusection(new food(Pizza));
+var section2 = new Menusection(new food(Snacks));
+var section3 = new Menusection(new food(Drinks));
+
+var Menu1 = new Menu(section1)
+var Menu2 = new Menu(section2)
+var Menu3 = new Menu(section3)
