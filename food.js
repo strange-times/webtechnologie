677a98d0a3b8cdf30b11a108b5f66a34992826ca
strange-times <fine_you_win@outlook.com>
2022-03-14 18:19:16 +0100

    added the outline of a DOM function in food

--- food.js
+++ food.js
@@ -3,6 +3,14 @@ class food {
     constructor(price) {
         this.price = price;
     }
+    show() {
+        var tag = document.createElement("th");
+        var text = createTextNode("");
+        tag.appendChild(text);
+        // define menu
+        var menu = document.getElementById("menu");
+        menu.appendChild(tag);
+    }
 }
 
 class PepporoniDeluxe extends food {
