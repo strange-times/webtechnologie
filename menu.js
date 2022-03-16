3e36048032569d6b24b7d667a650923c43f21181
Luc <luc2411b@gmail.com>
2022-03-16 15:51:39 +0100

    correctie

--- menu.js
+++ menu.js
@@ -1,25 +1,3 @@
-class Menu {
-    constructor (sections) {
-        this.sections = sections;
-    }
-    maketable = document.createElement
-}
-
-
-var section1 = new Menusection(new food(Pizza));
-var section2 = new Menusection(new food(Snacks));
-var section3 = new Menusection(new food(Drinks));
-
-var Menu1 = new Menu(section1)
-var Menu2 = new Menu(section2)
-var Menu3 = new Menu(section3)
-
-
-
-
-
-
-
 let btnPizza = document.createElement("button");
 btnPizza.innerHTML = "Pizza Menu";
 document.body.appendChild(btnPizza);
