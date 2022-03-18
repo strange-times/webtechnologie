5474f115e6ff749e47245d5ad58af4ecea365cc4
Luc <luc2411b@gmail.com>
2022-03-18 16:57:29 +0100

    samenvoegen menu

--- menu.js
+++ menu.js
@@ -1,3 +1,86 @@
+class Food {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Pizza extends Food{
+    constructor(name, price, spicy){
+    super(name, price);
+    this.spicy = spicy;
+    }
+}
+
+class Drink extends Food{
+    constructor(name, price, warm){
+    super(name, price);
+    this.warm = warm;
+    }
+}
+
+class Snack extends Food{
+    constructor(name, price, hot){
+    super(name, price);
+    this.hot = hot;
+    }
+}
+
+let Pepperoni = new Pizza("Pizza Pepperoni", 7,50, "No")
+let Chicken = new Pizza("Pizza Chicken", 8,50, "Yes")
+let Cola = new Drink("Coca Cola", 2,50, "No")
+let Coffee = new Drink("Large Coffee", 1,50, "Yes")
+let Icecream = new Snack("Chocolate Ice Cream", 2,50, "No")
+let Nuggets = new Snack("Chicken nuggets", 3,50, "Yes")
+
+
+
+class MenuSection {
+    constructor(foodItems) {
+        this.foodItems = foodItems;
+    }
+    show() {
+        for (let i = 0; i < this.foodItems.length; i++) {
+            let value1 = this.foodItems[i].name;
+            let value2 = this.foodItems[i].price;
+            let value3 = this.foodItems[i].spicy;
+            let row = document.createElement("tr");
+            let column1 = document.createElement("td")
+            column1.nodeValue = value1;
+            let column2 = document.createElement("td")
+            column2.nodeValue = value2;
+            let column3 = document.createElement("td")
+            column3.nodeValue = value3;
+            row.appendChild(column1);
+            row.appendChild(column2);
+            row.appendChild(column3);
+            let table = document.getElementById("table");
+            table.appendChild(row);
+        }
+    }
+}
+
+
+class menu {
+    constructor() {
+        let table = document.createElement('table');
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        table.id = "table";
+        document.body.appendChild(table)
+    }
+
+    set visibleSection(menuSection) {
+        this.visible = menuSection;
+        visible.show();
+    }
+}
+
+let m = new menu();
+
 let btnPizza = document.createElement("button");
 btnPizza.innerHTML = "Pizza Menu";
 document.body.appendChild(btnPizza);
@@ -28,11 +111,10 @@ let drinks = [
 ];
 
 btnPizza.addEventListener("click", () => {
-    function pizzaSection() {
-        let pepperoniDeluxe = new PepporoniDeluxe();
-        let fourCheese = new FourCheeses();
-        foodItems = [pepporoniDeluxe, fourCheese];
+    function pizzaSection(table, data) {
+        foodItems = [Pepperoni, Chicken];
         let pizzaSection = new MenuSection(foodItems);
+        menu.visibleSection(pizzaSection);
         pizzaSection.show();
     }
     function generateTableHead(table, data) {
@@ -122,23 +204,3 @@ btnDrinks.addEventListener("click", () => {
     generateTable(table, drinks);
 })
 
-class menu {
-    constructor() {
-        let table = document.createElement('table');
-        let thead = document.createElement('thead');
-
-        let tbody = document.createElement('tbody');
-
-        table.appendChild(thead);
-        table.appendChild(tbody);
-        table.id = "table";
-        document.body.appendChild(table)
-    }
-
-    set visibleSection(menuSection) {
-        this.visible = menuSection;
-        visible.run();
-    }
-}
-
-let m = new menu();
