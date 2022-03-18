32220bd08eaac2da41547dcd5cd6adf77c49e691
Luc <luc2411b@gmail.com>
2022-03-18 15:04:53 +0100

    food classes

--- menu2.js
+++ menu2.js
@@ -20,3 +20,39 @@ class menu {
 }
 
 let m = new menu();
+
+class Food {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+        this.spicy = spicy;
+    }
+}
+
+class Pizza extends Food{
+    constructor(name, price, spicy){
+    super(name, price);
+    this.spicy = spicy;
+    }
+}
+
+class Drink extends Food{
+    constructor(name, price, warm){
+    super(name, price);
+    this.warm = warm;
+    }
+}
+
+class Snack extends Food{
+    constructor(name, price, hot){
+    super(name, price);
+    this.hot = hot;
+    }
+}
+
+let Pepperoni = new Pizza("Pizza Pepperoni", 7,50, "No")
+let Chicken = new Pizza("Pizza Chicken", 8,50, "Yes")
+let Cola = new Drink("Coca Cola", 2,50, "No")
+let Coffee = new Drink("Large Coffee", 1,50, "Yes")
+let Icecream = new Snack("Chocolate Ice Cream", 2,50, "No")
+let Nuggets = new Snack("Chicken nuggets", 3,50, "Yes")
